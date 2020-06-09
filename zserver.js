c11c3937a6f30ad5858e5869dc02c758207881c6
Rory Jarrard <rory@crucibledevlabs.com>
2020-06-09 13:27:44 -0600

    add base server

---
+++ zserver.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+    console.log(`Server running on :${port}...`)
+})
