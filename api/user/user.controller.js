70bc3823e5a5d6d5a76442980f850011733d1818
Rory Jarrard <rory@crucibledevlabs.com>
2020-06-10 15:41:17 -0600

    implement login route

--- api/user/user.controller.js
+++ api/user/user.controller.js
@@ -1,6 +1,7 @@
 const db = require('../database')
 const { genSaltSync, hashSync } = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
 
 module.exports = {
     getUsers: async (req, res) => {
@@ -33,7 +34,7 @@ module.exports = {
             })
     },
 
-    createUser: async (req, res) => {
+    createUser: (req, res) => {
         const { first_name, last_name, email, password } = req.body
         const salt = genSaltSync(10)
         db.User.create({
@@ -53,6 +54,34 @@ module.exports = {
                     error = error.errors[0].message
                 }
                 return res.status(500).json({message: 'error creating user', error})
+            })
+    },
+
+    login: async (req, res) => {
+        const { email, password } = req.body
+
+        db.User.findOne({where: {email}})
+            .then((response) => {
+                if (!response) {
+                    return res.status(401).json({message: 'invalid credentials'})
+                }
+
+                const user = response.dataValues
+                const passwordIsValid = bcrypt.compareSync(password, user.password)
+                if (!passwordIsValid) {
+                    return res.status(401).json({
+                        messsage: 'invalid credentials'
+                    })
+                }
+
+                const accessToken = jwt.sign(user, process.env.JWT_ACCESS_SECRET)
+                return res.status(200).json({accessToken})
+            })
+            .catch((error) => {
+                if (error.errors && error.errors.length) {
+                    error = error.errors[0].message
+                }
+                return res.status(500).json({message: 'error logging in', error})
             })
     }
 }
--- api/user/user.router.js
+++ api/user/user.router.js
@@ -1,9 +1,10 @@
 const router = require('express').Router()
-const { getUsers, getUser, createUser } = require('./user.controller')
+const { getUsers, getUser, createUser, login } = require('./user.controller')
 const { authenticateToken } = require('../middleware/auth.middleware')
 
 router.get('/', authenticateToken, getUsers)
 router.get('/:id', authenticateToken, getUser)
 router.post('/', createUser)
+router.post('/login', login)
 
 module.exports = router
